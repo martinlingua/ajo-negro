6afdd7fb1d356b099c9cce4ef81878e6be7a8ce8
martinlingua <martin.lingua@gmail.com>
2026-08-18 10:48:29 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kaizen-v20260818133331';
+const CACHE='kaizen-v20260818134638';
 const STATIC=['./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(STATIC);}).then(function(){return self.skipWaiting();}));});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
